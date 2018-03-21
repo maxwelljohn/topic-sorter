abd5207b39fd750553da3da74b8eac2b7d59cbef
John Maxwell <dreamalgebra@gmail.com>
2018-03-21 16:36:15 -0700

    Load & display TSP solutions from U Waterloo site

--- tsp/tsp.py
+++ tsp/tsp.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from collections import defaultdict
 
+FIGSIZE = 6
+
 
 class TSPProblem(object):
     def __init__(self, filepath):
@@ -34,11 +36,15 @@ class TSPProblem(object):
             line = infile.readline().rstrip()
             assert line == '' or line == 'EOF'
 
-    def show(self):
-        xs = [node[0] for node in self.nodes]
-        ys = [node[1] for node in self.nodes]
+    def show(self, tour=None):
+        xs = np.array([node[0] for node in self.nodes])
+        ys = np.array([node[1] for node in self.nodes])
 
-        # Ensure x & y use same scale, for accurate distance visualization.
+        if tour:
+            xs = xs[tour.nodes]
+            ys = ys[tour.nodes]
+
+        # Ensure x & y use similar scale, for better distance visualization.
         x_spread = max(xs)-min(xs)
         x_mid = (max(xs)+min(xs))/2
         y_spread = max(ys)-min(ys)
@@ -47,15 +53,48 @@ class TSPProblem(object):
         bounds = matplotlib.transforms.Bbox(np.array([
             [x_mid-buff, y_mid-buff], [x_mid+buff, y_mid+buff]
         ]))
-        plt.figure(figsize=(6, 6))
+        plt.figure(figsize=(FIGSIZE, FIGSIZE))
         plt.plot(
-            xs, ys, 'ro', markersize=2, clip_box=bounds, scalex=True,
-            scaley=True
+            xs, ys, 'ko-' if tour else 'ro', markersize=1, clip_box=bounds,
+            scalex=True, scaley=True
         )
         plt.show()
+
+
+class TSPSolution(object):
+    def __init__(self, problem, filepath):
+        self.problem = problem
+
+        assert filepath.endswith('.tour')
+        with open(filepath, 'r') as infile:
+            self.info = defaultdict(list)
+            line = infile.readline()
+            while not line.startswith('TOUR_SECTION'):
+                parts = line.split(':')
+                self.info[parts[0].strip()].append(':'.join(parts[1:]).strip())
+                line = infile.readline()
+            assert self.info['TYPE'] == ['TOUR']
+            assert len(self.info['DIMENSION']) == 1
+            self.dimension = int(self.info['DIMENSION'][0])
+            assert self.dimension == self.problem.dimension
+
+            self.nodes = []
+            for i in range(self.dimension):
+                line = infile.readline().rstrip()
+                # .tour files are 1-indexed, but we 0-index nodes.
+                self.nodes.append(int(line)-1)
+            line = infile.readline().rstrip()
+            if line == '-1':
+                self.nodes.append(self.nodes[0])
+                line = infile.readline().rstrip()
+            assert line == '' or line == 'EOF'
+
+    def show(self):
+        self.problem.show(self)
 
 
 if __name__ == '__main__':
     import sys
     problem = TSPProblem(sys.argv[1])
-    problem.show()
+    solution = TSPSolution(problem, sys.argv[2])
+    solution.show()

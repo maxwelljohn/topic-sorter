839860e0a14b0285313f7c273fe04d509fcc7efd
John Maxwell <dreamalgebra@gmail.com>
2018-03-22 21:01:24 -0700

    OrderProblem/OrderSolution go to a separate file

---
+++ order_problem.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+from copy import copy
+
+
+class OrderProblem:
+    def __init__(self, dimension):
+        self.dimension = dimension
+        self.costs = -1 * np.ones(
+            (self.dimension, self.dimension), dtype=np.int
+        )
+
+
+class OrderSolution:
+    def __init__(self, problem):
+        self.problem = problem
+        self.dimension = self.problem.dimension
+        self.edges_added = np.zeros(
+            (self.dimension, self.dimension),
+            dtype=np.bool
+        )
+
+        self.node_degrees = np.zeros(self.dimension, dtype=np.int)
+        # Track which connected component each node belongs to.
+        # Components are named after an arbitrary node in the component.
+        self.connected_component = -1 * np.ones(self.dimension, dtype=np.int)
+        self.feasible_edges = np.zeros(
+            (self.dimension, self.dimension),
+            dtype=np.bool
+        )
+        for i in range(self.dimension):
+            self.feasible_edges[i, i+1:] = True
+        # Used for error checking.
+        self.valid_edges = copy(self.feasible_edges)
+
+        self.ensure_validity()
+        self.complete = False
+
+    def ensure_validity(self):
+        assert np.sum(self.edges_added * self.valid_edges, axis=(0, 1)) == \
+            np.sum(self.edges_added, axis=(0, 1))
+
+    def ensure_completion(self):
+        self.ensure_validity()
+        assert not self.feasible_edges.any()
+        cc_name = self.connected_component[0]
+        assert all(self.connected_component == cc_name)
+        assert self.complete
+
+    def add_edge(self, node_a, node_b):
+        node_a, node_b = sorted([node_a, node_b])
+
+        assert self.feasible_edges[node_a, node_b]
+        assert not self.edges_added[node_a, node_b]
+        self.feasible_edges[node_a, node_b] = False
+        self.edges_added[node_a, node_b] = True
+
+        for node in [node_a, node_b]:
+            assert self.node_degrees[node] < 2
+            self.node_degrees[node] += 1
+            if self.node_degrees[node] == 2:
+                # No more edges allowed for this node!
+                self.feasible_edges[node, :] = False
+                self.feasible_edges[:, node] = False
+
+        # This check needs to happen before updating edge feasibility.
+        # The TSPSolution class interprets
+        # not self.complete and not self.feasible_edges.any()
+        # to mean that we need just one final edge for a cycle.
+        if not self.feasible_edges.any():
+            self.complete = True
+            return
+
+        if self.connected_component[node_a] == -1 and \
+                self.connected_component[node_b] == -1:
+            self.connected_component[[node_a, node_b]] = node_a
+        elif self.connected_component[node_a] != -1 and \
+                self.connected_component[node_b] != -1:
+            assert self.connected_component[node_a] != \
+                self.connected_component[node_b]
+            swallower, swallowed = [
+                self.connected_component[node_a],
+                self.connected_component[node_b]
+            ]
+            self.connected_component[
+                [self.connected_component == swallowed]
+            ] = swallower
+            mask = self.connected_component == swallower
+            # For every node in the new, combined component...
+            for node in np.arange(self.dimension)[mask]:
+                # Edges to other nodes in the component are now infeasible.
+                self.feasible_edges[node, mask] = False
+        else:
+            if self.connected_component[node_a] == -1:
+                addition = node_a
+                swallower = self.connected_component[node_b]
+            else:
+                assert self.connected_component[node_b] == -1
+                addition = node_b
+                swallower = self.connected_component[node_a]
+            assert swallower != -1
+            self.connected_component[addition] = swallower
+            mask = self.connected_component == swallower
+            self.feasible_edges[addition, mask] = False
+            self.feasible_edges[mask, addition] = False
+
+        self.ensure_validity()
